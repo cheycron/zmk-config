f69b0e8489e3f607792539b4014ba440f71d63ba
Cheycron Blaine <cheycron@gmail.com>
2023-08-28 17:54:26 -0300

    new movement combos

--- config/includes/parameters.h
+++ config/includes/parameters.h
@@ -13,11 +13,15 @@
 &sk { release-after-ms = <500>; quick-release; };
 
 // Shortcuts
-#define C_UNDO LG(Z)
-#define C_REDO LS(LG(Z))
-#define C_CUT LG(X)
-#define C_COPY LG(C)
-#define C_PASTE LG(V)
+#define C_SELECT_ALL LC(A)
+#define C_SAVE  LC(S)
+#define C_FIND  LC(F)
+#define C_CUT LC(X)
+#define C_COPY LC(C)
+#define C_PASTE  LC(V)
+#define C_UNDO  LC(Z)
+#define C_REDO  LC(Y)
+// mic mute maybe?
 
 // Timeouts
 #define SLOW_COMBO_TIMEOUT 40
